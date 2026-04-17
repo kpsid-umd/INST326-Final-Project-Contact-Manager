471a14b1d0b27c87c1c04516ac61c16f178d7646
KPSid129 <padithaya05@gmail.com>
2026-04-17 14:08:46 -0400

    contact_manager.py
    General structure of the program. Need to add docustrings and parameters for project.

---
+++ contact_manager.py
@@ -0,0 +1,31 @@
+# This program is a contact manager that will allow users to manage their contacts
+# by adding, editing, and removing contacts. Each contact will list important information
+# like the contact's name, phone number, and email address.
+
+# Group Members: Siddharth Padithaya, Rayan Shah, and Raymond Dickschield
+class User:
+    """
+    
+    """
+    def __init__(self):
+        pass
+    def display_user_info():
+        pass
+    def edit_user_name():
+        pass
+
+class Contact:
+    def __init__(self):
+        pass
+    def update_info():
+        pass
+    def get_info():
+        pass
+
+class Addressbook:
+    def __init__(self):
+        pass
+    def add_contact():
+        pass
+    def edit_contact():
+        pass
