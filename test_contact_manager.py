e59a9b1394c48f38c924d491f0b56739dc730fda
kpsid-umd <kpsid@terpmail.umd.edu>
2026-04-30 12:15:17 -0400

    Added more test cases with different scenarios

--- test_contact_manager.py
+++ test_contact_manager.py
@@ -6,7 +6,7 @@ class TestContactManager(unittest.TestCase):
         user = User("Matthew")
         self.assertEqual(user.name, "Matthew")
 
-    def test_display_user_info(self):
+    def test_edit_user_name(self):
         user = User("Matthew")
         user.edit_user_name("Matt")
         self.assertEqual(user.name, "Matt")
@@ -42,7 +42,7 @@ class TestContactManager(unittest.TestCase):
         contact = Contact("Alex", "301-234-2853", "alex@gmail.com")
         book.add_contact(contact)
 
-        self.assertEqual(book.find_contact("Alex"), contact())
+        self.assertEqual(book.find_contact("Alex"), contact)
 
     def test_find_contact_not_found(self):
         book = AddressBook()
@@ -67,11 +67,57 @@ class TestContactManager(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             book.edit_contact("Ray", new_name = "Jay")
-            
+    
+    def test_remove_contact_found(self):
+        book = AddressBook()
+        contact = Contact("Jack", "240-223-3481", "ja1@gmail.com")
+
+        book.add_contact(contact)
+        book.remove_contact("Jack")
+        self.assertEqual(len(book.contacts), 0)
+    
+    def test_remove_contact_not_found(self):
+        book = AddressBook()
+        with self.assertRaises(ValueError):
+            book.remove_contact("Contact could not be found")
+    
+    def test_export_contacts(self):
+        book = AddressBook()
+        contact = Contact("Emily", "940-334-4579", "emil@gmail.com")
+        book.add_contact(contact)
+
+        file_name = "My Contacts.txt"
+        book.export_contacts(file_name)
+
+        with open(file_name, "r") as file:
+            self.assertIn("Emily", file.read())
+
+    def test_export_contacts_empty(self):
+        book = AddressBook()
+        
+        with self.assertRaises(ValueError):
+            book.export_contacts("Empty.txt")
+    
+    def test_get_contact_info(self):
+        contact = Contact("Micheal", "459-344-4931", "m21@yahoo.com")
+        info = contact.get_info()
+
+        self.assertIn("Micheal", info)
+        self.assertIn("459-344-4931", info)
+        self.assertIn("m21@yahoo.com", info)
+    
+    def test_missing_values_contact_info(self):
+        with self.assertRaises(TypeError):
+            Contact("Jason", "345-344-8595")
 
     
+    def test_multiple_contact_fields(self):
+        contact = Contact("Sarah", "240-223-3481", "sarah1@gmail.com")
+        contact.update_info(name = "Sarah J", email = "Saraj1@gmail.com")
 
+        self.assertEqual(contact.name, "Sarah J")
+        self.assertEqual(contact.email, "Saraj1@gmail.com")
+        self.assertEqual(contact.phone_number, "240-223-3481")
         
-
 if __name__ == "__main__":
     unittest.main()
